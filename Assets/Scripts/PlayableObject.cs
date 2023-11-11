1f661670b5b75b79bfeaab2d980a5aa008f97dc2
Christian Yang <kychris1999@hotmail.com>
2023-11-11 15:00:07 -0800

    PLayable object test update

--- Assets/Scripts/PlayableObject.cs
+++ Assets/Scripts/PlayableObject.cs
@@ -1,28 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 [RequireComponent(typeof(AudioSource))]
 
 public class PlayableObject : MonoBehaviour
 {
   // Start is called before the first frame update
-  private AudioSource audioSource;
+  public AudioSource audioSource1;
+  public AudioSource audioSource2;
+  public TextMeshPro tmp;
+  public float cooldownTime = 0.3f; // Cooldown time in seconds
+  private float lastCollisionTime = -1f; // Time of the last collision
 
   void Start() {
     // Get the AudioSource component
-    audioSource = GetComponent<AudioSource>();
+    //audioSource1 = GetComponent<AudioSource>();
+    //audioSource2 = GetComponent<AudioSource>();
   }
 
   void OnTriggerEnter(Collider other) {
     // Play the sound when another object enters the trigger
-      audioSource.Play();
+      //audioSource.Play();
   }
 
   //Optional: Use OnCollisionEnter for physical collisions
    void OnCollisionEnter(Collision collision) {
-    if (!audioSource.isPlaying) {
-      audioSource.Play();
+   
+      string otherObjectName = collision.gameObject.name;
+
+      tmp.text = otherObjectName;
+
+    if (Time.time - lastCollisionTime >= cooldownTime) {
+      if (otherObjectName.Contains("Joint 2") || otherObjectName.Contains("Joint 1")) {
+        if (!audioSource2.isPlaying) { 
+          audioSource1.Play();
+        }
+      } else {
+        if (!audioSource1.isPlaying) { 
+          audioSource2.Play();
+        }
+      }
     }
+      // Now you can use otherObjectName as needed
+      Debug.Log("Collided with: " + otherObjectName);
   }
 }
