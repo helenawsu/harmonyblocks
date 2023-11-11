ad260da1d6c3a4901864e3f40f4b7506c2158da4
Christian Yang <kychris1999@hotmail.com>
2023-11-11 10:58:12 -0800

    Add Playable object script and add test sounds

--- Assets/Scripts/PlayableObject.cs
+++ Assets/Scripts/PlayableObject.cs
@@ -2,17 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
+
 public class PlayableObject : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  // Start is called before the first frame update
+  private AudioSource audioSource;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+  void Start() {
+    // Get the AudioSource component
+    audioSource = GetComponent<AudioSource>();
+  }
+
+  void OnTriggerEnter(Collider other) {
+    // Play the sound when another object enters the trigger
+      audioSource.Play();
+  }
+
+  //Optional: Use OnCollisionEnter for physical collisions
+   void OnCollisionEnter(Collision collision) {
+    if (!audioSource.isPlaying) {
+      audioSource.Play();
     }
+  }
 }
